1b2a7ad7b045a951ba58f41f9ddedc1d2e4b9b94
SANGARIGURU <sangariguru03@gmail.com>
2022-03-02 16:27:47 +0530

    Dineout Passport

--- src/components/dineoutPassport/DineOutPassport.jsx
+++ src/components/dineoutPassport/DineOutPassport.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { DropDownNavbar } from '../navbar/DropDownNavbar';
+import React from "react";
+import { DropDownNavbar } from "../navbar/DropDownNavbar";
+import { DineOutGif } from "./DineOutGif";
+import styles from "./dineOutPassport.module.css";
+import { DineOutPassportExperience } from "./DineOutPassportExperience";
+import { DineOutPassportMember } from "./DineOutPassportMember";
+import { DineOutPassRegister } from "./DineOutPassRegister";
 
+import { GetToKnow } from "./GetToKnow";
 export const DineOutPassport = () => {
-  return (
+	return (
 		<div>
-			<DropDownNavbar />
-			DineOutPassport
+			<div>
+				<DropDownNavbar />
+			</div>
+
+			<div className={styles.dineOutAlignMain}>
+				<DineOutGif />
+				<DineOutPassportMember />
+				<DineOutPassportExperience />
+
+				<DineOutPassRegister />
+				<GetToKnow />
+			</div>
 		</div>
-  );
-}
+	);
+};
