369cfc46be4bdbb1de94eb224309e19cdbc73f1b
sudhanshu2710 <sudhanshu.singh2710@gmail.com>
2022-03-03 15:43:07 +0530

    first

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,55 +7,67 @@ import styles from "./navbar.module.css";
 import { LogInNavbar } from "./LogInNavbar";
 
 export const Navbar = () => {
-	const places = useContext(NavbarContext);
-	return (
-		<div className={styles.navbarMain}>
-			<div>
-				<img
-					src="https://im1.dineout.co.in/images/uploads/misc/2019/Jul/25/website-logo.png"
-					alt="dineoutlogo"
-				/>
-			</div>
-			<div>
-				<Autocomplete
-					disablePortal
-					id="combo-box-demo"
-					options={places}
-					sx={{
-						width: 220,
-					}}
-					renderInput={(params) => (
-						<TextField
-							{...params}
-							placeholder="Location"
-							size="small"
-						/>
-					)}
-				/>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/">Home</Link>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/bookatable">Book a Table</Link>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/dineoutpay">Dineout Pay</Link>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/dineoutpassport">Dineout Passport</Link>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/events">Events</Link>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/supersaver">Super Saver</Link>
-			</div>
-			<div>
-				<Link className={styles.anchorNavbar} to="/blog">Blog</Link>
-			</div>
-			{/* <button className={styles.navbarLoginButton}>Login</button> */}
-			<LogInNavbar />
-		</div>
-	);
+  const places = useContext(NavbarContext);
+  return (
+    <React.Fragment>
+      <div className={styles.navbarMain}>
+        <div>
+          <img
+            src="https://im1.dineout.co.in/images/uploads/misc/2019/Jul/25/website-logo.png"
+            alt="dineoutlogo"
+          />
+        </div>
+        <div>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={places}
+            sx={{
+              width: 220,
+            }}
+            renderInput={(params) => (
+              <TextField {...params} placeholder="Location" size="small" />
+            )}
+          />
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/">
+            Home
+          </Link>
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/bookatable">
+            Book a Table
+          </Link>
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/dineoutpay">
+            Dineout Pay
+          </Link>
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/dineoutpassport">
+            Dineout Passport
+          </Link>
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/events">
+            Events
+          </Link>
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/supersaver">
+            Super Saver
+          </Link>
+        </div>
+        <div>
+          <Link className={styles.anchorNavbar} to="/blog">
+            Blog
+          </Link>
+        </div>
+        {/* <button className={styles.navbarLoginButton}>Login</button> */}
+        <LogInNavbar />
+      </div>
+    </React.Fragment>
+  );
 };
