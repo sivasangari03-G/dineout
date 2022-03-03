2d680230b352cc736f2a2c0f7235c30c3e1bd5ab
SANGARIGURU <sangariguru03@gmail.com>
2022-03-03 16:10:33 +0530

    corrected app file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,33 @@
-
-import { useEffect } from "react";
 import "./App.css";
+import { useEffect } from "react";
 import { NavbarRouterComponent } from "./routes/NavbarRouterComponent";
 import { fetchCartData, sendCartData } from "./store/cart-action";
 import { useDispatch, useSelector } from "react-redux";
 let initial = true;
 function App() {
-  const cart = useSelector((state) => state.cart);
+	const cart = useSelector((state) => state.cart);
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCartData());
-  }, [dispatch]);
-  console.log(cart);
-  useEffect(() => {
-    //console.log("is data being sent?");
-    //----------------------------------------------thunk----------------------------------------------
-    // if (initial) {
-    //   initial = false;
-    //   return;
-    // }
-    //console.log("setting data");
-    if (cart.changed) dispatch(sendCartData(cart));
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchCartData());
+	}, [dispatch]);
+	console.log(cart);
+	useEffect(() => {
+		//console.log("is data being sent?");
+		//----------------------------------------------thunk----------------------------------------------
+		// if (initial) {
+		//   initial = false;
+		//   return;
+		// }
+		//console.log("setting data");
+		if (cart.changed) dispatch(sendCartData(cart));
 
-    //----------------------------------------------thunk----------------------------------------------
-  }, [cart, dispatch]);
-  return (
-    <div className="App">
-      <NavbarRouterComponent />
-    </div>
-  );
-
-import "./App.css";
-import { NavbarRouterComponent } from "./routes/NavbarRouterComponent";
-
-function App() {
+		//----------------------------------------------thunk----------------------------------------------
+	}, [cart, dispatch]);
 	return (
 		<div className="App">
 			<NavbarRouterComponent />
 		</div>
 	);
 }
-
 export default App;
