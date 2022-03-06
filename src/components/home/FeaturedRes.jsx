fa031388de9046a0084e56c9c4f8b34b393de835
sudhanshu2710 <sudhanshu.singh2710@gmail.com>
2022-03-06 09:35:24 +0530

    featureLinked

--- src/components/home/FeaturedRes.jsx
+++ src/components/home/FeaturedRes.jsx
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "../dineoutPassport/carousel.css";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import { Link } from "react-router-dom";
+import { CardDisplay } from "./CardDisplay";
 export const FeaturedRes = () => {
   // const settings = {
   // 	focusOnSelect: true,
@@ -36,57 +37,65 @@ export const FeaturedRes = () => {
           {data.map((elem) => {
             return (
               <div key={elem.id}>
-                <Link to="/" className={styles.featuredLink}>
-                  <div key={elem.id} className={styles.restaurantCartsMain}>
-                    <div>
-                      <div className={styles.imgMain}>
-                        <img
-                          className={styles.resImg}
-                          src={elem.image}
-                          alt="hotel images"
-                        />
-                      </div>
-                      <div className={styles.resAllMain}>
-                        <div className={styles.nameAddressArea}>
-                          <div className={styles.name}>{elem.name}</div>
-                          <div className={styles.addressArea}>
-                            {elem.address}, {elem.area}
-                          </div>
-                        </div>
-                        <div className={styles.rating}>{elem.rating}</div>
-                      </div>
+                <div key={elem.id} className={styles.restaurantCartsMain}>
+                  <div>
+                    <div
+                      style={{
+                        border: "1px solid rgba(0, 0, 0, 0.05)",
+                        marginLeft: "10px",
+                        marginRight: "10px",
+                      }}
+                    >
+                      <CardDisplay elem={elem} />
+                    </div>
 
+                    {/* <div className={styles.imgMain}>
+                      <img
+                        className={styles.resImg}
+                        src={elem.image}
+                        alt="hotel images"
+                      />
+                    </div>
+                    <div className={styles.resAllMain}>
+                      <div className={styles.nameAddressArea}>
+                        <div className={styles.name}>{elem.name}</div>
+                        <div className={styles.addressArea}>
+                          {elem.address}, {elem.area}
+                        </div>
+                      </div>
+                      <div className={styles.rating}>{elem.rating}</div>
+                    </div> */}
+
+                    <div
+                      style={{
+                        borderBottom: "1px solid rgba(0, 0, 0, 0.05)",
+                        marginLeft: "20px",
+                        marginRight: "20px",
+                      }}
+                    ></div>
+                    <div
+                      style={{
+                        color: "#54BD8A",
+                        fontWeight: "600",
+                        display: "flex",
+                        alignItems: "center",
+                        paddingLeft: "20px",
+                        marginTop: "10px",
+                        paddingBottom: "20px",
+                      }}
+                    >
+                      <LocalOfferIcon fontSize="small" />
                       <div
                         style={{
-                          borderBottom: "1px solid rgba(0, 0, 0, 0.05)",
-                          marginLeft: "20px",
-                          marginRight: "20px",
-                        }}
-                      ></div>
-                      <div
-                        style={{
-                          color: "#54BD8A",
-                          fontWeight: "600",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "20px",
-                          marginTop: "10px",
-                          paddingBottom: "20px",
+                          paddingLeft: "10px",
+                          fontSize: "14px",
                         }}
                       >
-                        <LocalOfferIcon fontSize="small" />
-                        <div
-                          style={{
-                            paddingLeft: "10px",
-                            fontSize: "14px",
-                          }}
-                        >
-                          Flat 10% Off the Total Bill
-                        </div>
+                        Flat 10% Off the Total Bill
                       </div>
                     </div>
                   </div>
-                </Link>
+                </div>
               </div>
             );
           })}
--- src/contexts/RestaurantNeatYouContext.jsx
+++ src/contexts/RestaurantNeatYouContext.jsx
@@ -1,30 +1,32 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export const RestaurantNeatYouContext = createContext();
 
 export const RestaurantNeatYouContextProvider = ({ children }) => {
-	var [resData, setResData] = useState();
+  var [resData, setResData] = useState();
+  //let city = useSelector((store) => store.RestaurantReducer.city);
+  // console.log(city, "this city is coming from home page");
+  useEffect(() => {
+    fetch(`http://localhost:8000/posts`, {
+      method: "GET",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        // console.log(json);
+        display(json);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-	useEffect(() => {
-		fetch(`http://localhost:8000/posts`, {
-			method: "GET",
-			headers: { "Content-type": "application/json;charset=UTF-8" },
-		})
-			.then((response) => response.json())
-			.then((json) => {
-				// console.log(json);
-				display(json);
-			})
-			.catch((err) => console.log(err));
-	}, []);
-
-	function display(json) {
-		// console.log(json);
-		setResData(json);
-	}
-	return (
-		<RestaurantNeatYouContext.Provider value={resData}>
-			{children}
-		</RestaurantNeatYouContext.Provider>
-	);
+  function display(json) {
+    // console.log(json);
+    setResData(json);
+  }
+  return (
+    <RestaurantNeatYouContext.Provider value={resData}>
+      {children}
+    </RestaurantNeatYouContext.Provider>
+  );
 };
